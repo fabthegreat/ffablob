33c63861263f802b0848dea8792386090ff0177c
Firstname Lastname <email_address>
2018-05-12 13:46:26 +0200

    table FFA vers CSV

--- convertFFA2kikourou.py
+++ convertFFA2kikourou.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 
 import urllib
-#url = urllib.urlopen("http://bases.athle.com/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=205707&frmepreuve=1%2f2+Marathon+TC")
-url = urllib.urlopen("/home/ftg/Documents/Programmation/python/ffablob/CompetitionsResults.html")
+url_base="http://bases.athle.com/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=205707&frmepreuve=1%2f2+Marathon+TC"
+#url = urllib.urlopen("/home/ftg/Documents/Programmation/python/ffablob/CompetitionsResults.html")
+url_base="http://bases.athle.com/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=205572&frmepreuve=10+Km+Route+TC"
 
 
-soup = BeautifulSoup(url,"lxml")
 
 def format_runner(runner):
     runner_=[]
@@ -25,25 +25,40 @@ def format_runner(runner):
 
 # loop over each page of results
 #
-
 results=[]
-for a in soup.find_all('tr'): #loop over all tr tags
-    if a.td.get_attribute_list('class')[0] in ('datas0','datas1'): # detect all tr wherein td has a td with datas0 or 1 class
-        runner=[]
-        for b in a.find_all('td'):
-            if b.get_attribute_list('class')[0] in ('datas0','datas1'):
-                runner.append(b.contents)
-#        print runner
-        results.append([i for i in format_runner(runner)])
-#        results.append(runner)
 
-for runner in results:
-#    print("\n")
-#    print("###################")
-#    print("Runner " + str(runner[0]))
-#    print("###################")
-    print runner
+url = urllib.urlopen(url_base)
+soup = BeautifulSoup(url,"lxml")
+total_page_number=soup.find_all('select')[0].contents[0].string.split('/')[1].split('<')[0].strip()
 
+print "Total page number: " + str(total_page_number)
+for page_number in range(int(total_page_number)):
+    url_page = url_base + "&frmposition=" + str(page_number)
+    print "Page number: " + str(page_number)
 
+    url = urllib.urlopen(url_page)
+    soup = BeautifulSoup(url,"lxml")
+
+    for a in soup.find_all('tr'): #loop over all tr tags
+        if a.td.get_attribute_list('class')[0] in ('datas0','datas1'): # detect all tr wherein td has a td with datas0 or 1 class
+            runner=[]
+            for b in a.find_all('td'):
+                if b.get_attribute_list('class')[0] in ('datas0','datas1'):
+                    runner.append(b.contents)
+    #        print runner
+            results.append([i for i in format_runner(runner)])
+    #        results.append(runner)
+
+with open('test.csv', 'w') as f:
+    print "class;temps;nom;cat;sexe;club"
+    f.write("class;temps;nom;cat;sexe;club\n")
+    for runner in results:
+    #    print("\n")
+    #    print("###################")
+    #    print("Runner " + str(runner[0]))
+    #    print("###################")
+        print runner[0] + ";" + runner[1] + ";" + runner[2] + ";" + runner[3][:2] + ";" + runner[3][2:] + ";" + runner[4] + ";"
+        f.write(runner[0] + ";" + runner[1] + ";" + runner[2] + ";" + runner[3][:2] + ";" + runner[3][2:] + ";" + runner[4] + ";\n")
+f.closed
 
 
