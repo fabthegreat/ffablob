1dc5b4f44c0ddc974fbb9fcc7d3e555da5a1ab65
Firstname Lastname <email_address>
2018-06-13 08:08:39 +0200

    Avancées base de données

--- core/convertFFA2kikourou.py
+++ core/convertFFA2kikourou.py
@@ -3,6 +3,8 @@ import re
 import urllib.request
 import random
 from datetime import datetime,timedelta
+import sys
+
 import bd
 
 def time_converter(time):
@@ -53,6 +55,7 @@ def cleanup(str_chrono):
         return str_chrono
 
 class Chrono:
+    # verifier tous les formats...
     def __init__(self,str_chrono):
         str_chrono = cleanup(str_chrono)
         try:
@@ -69,16 +72,18 @@ class Chrono:
             else:
                 return "{:02}h{}'{}''".format(int(s//3600),int(s % 3600 //60),int(s % 60))
 
-
-
 class Runner:
     """Runner"""
-    def __init__(self,runner_ID="unknown",name="unknown",club="unknown",category="unknown"):
+    def __init__(self,runner_ID='',name="unknown",club="unknown",category="XX",gender="X"):
         self.runner_ID=runner_ID
         self.bilans=[]
         self.name = name
         self.club = club
         self.category = category
+        self.gender = gender
+
+    def __str__(self):
+        return 'ID: '+self.runner_ID+' name: '+self.name+' club: '+self.club+' category: '+self.category+' gender: '+self.gender
 
     def fetch_bilans(self,race_type=10):
         url_runner="http://bases.athle.com/asp.net/athletes.aspx?base=bilans&seq=" + str(strtohex(self.runner_ID))
@@ -107,21 +112,25 @@ class Race:
     def __init__(self,url):
         self.urlFFA = url
         self.results = []
-        self.race_ID=self.parse_url(self.urlFFA)
+        self.race_ID,self.epreuve=self.parse_url(self.urlFFA)
         self.errors = []
 
     def append_runner(self,runner,chrono):
         self.results.append({'temps':chrono,'runner':runner})
 
     def parse_url(self,urlFFA):
-        return urlFFA.split("frmcompetition=")[1].split("&")[0]
+        race_ID=urlFFA.split("frmcompetition=")[1].split("&")[0]
+        epreuve=urlFFA.split("frmepreuve=")[1]
+        return race_ID,epreuve
 
     def subrace_bilans(self,rankinf,ranksup,years_list,race_type):
+        # revoir plutot en fonction des coureurs dans la bdd
         results_=[]
         results_runner = []
 
         for i,r in enumerate(self.results[rankinf-1:ranksup]):
-            if r['runner'].runner_ID != 'unknown':
+            print(r['runner'].name)
+            if r['runner'].runner_ID:
                 chrono = r['temps']
                 runner = r['runner']
                 runner.fetch_bilans(race_type)
@@ -141,7 +150,8 @@ class Race:
         runner = Runner()
         if line_runner[0][0].string == '-' or line_runner[2][0].string.split('(')[0].strip()=='Participant Invalide':
             runner.name = 'nom inconnu'
-            runner.category = 'XXX'
+            runner.category = 'XX'
+            runner.gender = 'X'
             runner.club='club inconnu'
             runner.runner_ID=''
             cr = self.results[-1]['temps'] #le chrono du
@@ -153,7 +163,8 @@ class Race:
                 runner.runner_ID=''
 
             runner.name=line_runner[2][0].string.split('(')[0].strip()
-            runner.category=line_runner[6][0].string # runner category
+            runner.category=line_runner[6][0].string[:2] # runner category
+            runner.gender=line_runner[6][0].string[2:] # runner category
             cr = Chrono(line_runner[1][0].string)
             if line_runner[3][0].string is None:
                 runner.club=''
@@ -166,38 +177,52 @@ class Race:
         return runner, cr
 
     def extract_runners(self):
-        soup=extract_soup(self.urlFFA)
-        total_page_number=soup.find_all('select')[0].contents[0].string.split('/')[1].split('<')[0].strip()
+        # detect if race is already stored in database and extract only if
+        # necessary
+        if bd.check_race_new(self):
+            print('Race already in database, will retrieve existing datas...')
+            bd.fetch_race_results(self)
+        else:
+            print('Race not in database, need to download from FFA site...')
+            bd.insert_race(self) #injects race in table
+            soup=extract_soup(self.urlFFA)
+            total_page_number=soup.find_all('select')[0].contents[0].string.split('/')[1].split('<')[0].strip()
 
-        # retrieve maximum pas number if any
+            # retrieve maximum pas number if any
 
-        # because when page number is equal to 1, no html tag with no total
-        # page number
-        try:
-            total_page_number = int(total_page_number)
-        except ValueError:
-            total_page_number = 1
+            # because when page number is equal to 1, no html tag with no total
+            # page number
+            try:
+                total_page_number = int(total_page_number)
+            except ValueError:
+                total_page_number = 1
 
-        for page_number in range(total_page_number): # check first value in
-            url_page = self.urlFFA + "&frmposition=" + str(page_number)
-            soup=extract_soup(url_page)
+            for page_number in range(total_page_number): # check first value in
+                url_page = self.urlFFA + "&frmposition=" + str(page_number)
+                soup=extract_soup(url_page)
 
-            for a in soup.find_all('tr'): #loop over all tr tags
-                if a.td.get_attribute_list('class')[0] in ('datas0','datas1'): 
-                    runner_tmp=[]
-                        # detect all tr wherein td has a td with datas0 or 1 class
-                    for b in a.find_all('td'):
-                        if b.get_attribute_list('class')[0] in ('datas0','datas1'):
-                            # loop within a runner row
-                            runner_tmp.append(b.contents)
-                    runner,cr = self.fill_runner(runner_tmp)
-                    self.append_runner(runner, cr)
+                for a in soup.find_all('tr'): #loop over all tr tags
+                    if a.td.get_attribute_list('class')[0] in ('datas0','datas1'): 
+                        runner_tmp=[]
+                            # detect all tr wherein td has a td with datas0 or 1 class
+                        for b in a.find_all('td'):
+                            if b.get_attribute_list('class')[0] in ('datas0','datas1'):
+                                # loop within a runner row
+                                runner_tmp.append(b.contents)
+                        runner,cr = self.fill_runner(runner_tmp)
+                        self.append_runner(runner, cr)
+                        bd.insert_race_results(runner,self)
+
+    def populate(self):
+        pass
 
     def write2csv(self,path_output,filename):
         with open(path_output + filename + '.csv', 'w',encoding='utf8') as f:
             f.write("class;temps;nom;cat;sexe;club\n")
             for i,rank in enumerate(self.results):
-                f.write(str(i+1) + ";" + rank['temps'] + ";" + rank['runner'].name + ";" + rank['runner'].category[:2] + ";" + rank['runner'].category[2:] + ";" + rank['runner'].club + ";\n")
+                f.write(str(i+1) + ";" + str(rank['temps']) + ";" +
+                        rank['runner'].name + ";" + rank['runner'].category +
+                        ";" + rank['runner'].gender + ";" + rank['runner'].club + ";\n")
             f.close()
 
 def convert(url_base,path_output):
@@ -221,9 +246,9 @@ def analyse(url_base,rankinf,ranksup,race_type=10,
             ranksup=check_ranks(rankinf,ranksup)[2]
             race=Race(url_base)
             race.extract_runners()
-
-            return (True,race.subrace_bilans(rankinf,ranksup,
-                                             years_list,race_type))
+            bilans_=race.subrace_bilans(rankinf,ranksup,years_list,race_type)
+            print(bilans_)
+            return (True,bilans_)
         else:
             return (False,"Vérifiez les rangs")
     else:
@@ -231,4 +256,4 @@ def analyse(url_base,rankinf,ranksup,race_type=10,
 
 if __name__ == "__main__":
     """pour execution si demarrage en stand alone"""
-    analyse('http://bases.athle.com/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=184050&frmepreuve=30+Km',45,75,10)
+    analyse('http://bases.athle.com/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=184050&frmepreuve=30+Km',1,111,10)
